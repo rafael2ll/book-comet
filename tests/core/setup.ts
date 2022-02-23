fb2c356f44300f211e230d3fd2eb7ba9f7349c92
Rafael <rafael.valentim@zup.com.br>
2022-02-23 10:10:33 -0300

    promises all instead of blocking array

--- tests/core/setup.ts
+++ tests/core/setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable no-restricted-syntax */
 import { connectToDatabase } from '@db/db'
 import { connection } from 'mongoose'
@@ -8,10 +9,12 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     const collections = await connection.collections
+    const promises = []
     const collectionsKeys = Object.keys(collections)
     for (const key of collectionsKeys) {
         const collection = collections[key]
         // @ts-ignore
-        await collection.deleteMany()
+        promises.push(collection.deleteMany())
     }
+    await Promise.all(promises)
 })
